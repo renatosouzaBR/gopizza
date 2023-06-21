b24ef48b77de37b2a3c70ed9b8f10358ccf77c67
Renato Souza <natoicm@gmail.com>
2023-06-21 17:35:01 -0300

    chore: move gopizza project to its own repo

--- projects/myshopping/src/components/ButtonText/styles.ts
+++
@@ -1,9 +0,0 @@
-import styled from 'styled-components/native';
-
-export const Title = styled.Text`
-  font-size: 14px;
-  font-family: ${({ theme }) => theme.FONTS.MEDIUM};
-  color: ${({ theme }) => theme.COLORS.BLACK};
-  margin-left: 7px;
-`;
-
